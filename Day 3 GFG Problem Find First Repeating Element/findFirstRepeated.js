99fecf4f9a4e247163567e82b94cf2811c5d8c22
Abner Navarro <abner.navarro98@gmail.com>
2022-09-12 00:48:33 -0400

    Day 3 GFG Code Problem: Find First Repeating Element
    Find First Repeating Element Geek for Geeks Code Problem using hash Tables in Js.
    
    Passed 142 / 142 test cases

---
+++ Day 3 GFG Problem Find First Repeating Element/findFirstRepeated.js
@@ -0,0 +1,32 @@
+// PROBLEM DESCRIPTION
+
+// Given an array arr[] of size n, find the first repeating element. 
+// The element should occurs more than once and the index of its first occurrence should be the smallest.
+
+// The task: 
+// Complete the function firstRepeated() which takes arr and n as input parameters and return the position
+// of the first repeating element. If there is no such element, return -1.
+// The position you return should be according to 1 - based indexing. 
+
+class Solution {
+    // Function to return the position of the first repeating element.
+    firstRepeated(arr, n) {
+        // your code here
+        let hMap = new Map();
+        let min = -1;
+        for (let i = 0; i < n; i++) {
+            if (hMap.has(arr[i])) {
+                if (min === -1) min = n;
+                let index = hMap.get(arr[i]);
+                if (index < min) min = index;
+            } else {
+                let index = i + 1;
+                hMap.set(arr[i], index)
+            }
+        }
+        return min
+    }
+}
+
+let s = new Solution();
+console.log(s.firstRepeated([1, 5, 3, 4, 3, 5, 6], 7));
